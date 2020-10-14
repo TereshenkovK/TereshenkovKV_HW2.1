17985062fbf8e793c87fc700ba9f281972e97c63
sten <sten79@gmail.com>
2020-10-14 17:21:47 +0300

    Initial Commit

---
+++ TereshenkovKV_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TereshenkovKV_HW2.1
+//
+//  Created by Admin on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
